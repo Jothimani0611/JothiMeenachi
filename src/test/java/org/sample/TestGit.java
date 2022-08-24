6a37e30b92bbbc6411a9fef08c6e48b2cfd58dff
Jothi0611 <jothi611mani@gmail.com>
2022-08-24 08:15:22 -0500

    Pass the value to the git

---
+++ src/test/java/org/sample/TestGit.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class TestGit {
+
+	private void fun1() {
+		System.out.println("I am fun1");
+	}
+	private void fun2() {
+		System.out.println("I am fun2");
+
+	}
+	private void fun3() {
+	System.out.println("I am fun3");
+
+	}
+}
